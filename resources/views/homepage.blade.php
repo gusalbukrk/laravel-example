a707aeebcfb955c158720bbe70ce283ae12ec366
gusalbukrk <gusalbukrk@gmail.com>
2023-08-04 08:58:23 -0300

    add `layout` component

--- resources/views/homepage.blade.php
+++ resources/views/homepage.blade.php
@@ -1,7 +1,9 @@
-@auth
-    <h1>Welcome, {{auth()->user()->email}}!</h1>
-    <a href="/logout">Log Out</a>
-@else
-    <a href="/login">Log In</a>
-    <a href="/signup">Sign Up</a>
-@endauth
+<x-layout title="Homepage">
+    @auth
+        <h1>Welcome, {{auth()->user()->email}}!</h1>
+        <a href="/logout">Log Out</a>
+    @else
+        <a href="/login">Log In</a>
+        <a href="/signup">Sign Up</a>
+    @endauth
+</x-layout>
